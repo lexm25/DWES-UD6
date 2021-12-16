78401a42c4195c0de13191ec76e74d01b2747688
lexm25 <alejandro.martin.alvarez.al@iespoligonosur.org>
2021-12-16 10:53:12 +0100

    clase

---
+++ coche.php
@@ -0,0 +1,12 @@
+<?php 
+class Coche {
+    public $color;
+    public $potencia;
+    public $marca;
+}
+
+$miCoche = new Coche();
+$miCoche->color = 'rojo';
+$miCoche->potencia = 120;
+$miCoche->marca = 'audi';
+?>
