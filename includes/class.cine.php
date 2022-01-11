308d3f6f9530fa1da0e46155534e25a0ef18d698
Lex <alejandromartinalvarez2000@gmail.com>
2022-01-11 19:51:54 +0100

    casa

--- includes/class.cine.php
+++ includes/class.cine.php
@@ -1,5 +1,25 @@
 <?php 
-    class Cine{
+    class Cine extends LocalComercial{
+        private int $aforoSala;
 
+        public function __construct($aforoSala){
+            $this->aforoSala = $aforoSala;
+        }
+
+        public function __set($name, $value){
+            if($value > 0){
+                $this->name = $value;
+            }
+            else{
+                echo "ERROR, el aforo debe ser mayor a 0";
+            }
+        }
+        
+        public function __toString(){
+            echo "<p>" . "datoslocal" . "Aforo: " . $this->aforoSala . "<br></p>";
+
+        }
     } 
+
+  
 ?>
--- includes/class.localcomercial.php
+++ includes/class.localcomercial.php
@@ -1,1 +1,16 @@
-<?php ?>
+<?php 
+    class LocalComercial{
+        private string $razonSocial;
+        private string $numLicencia;
+
+        public function __construct($razonSocial,$numLicencia){
+            $this->razonSocial = $razonSocial;
+            $this->numLicencia = $numLicencia;
+        }
+
+        public function __toString(){
+            echo "<p>El local está situado en la ciudad" . $this->ciudad . " en la calle " . $this->calle . ".El local dispone de " . $this->numPlantas . "</p>Razón Social:" .  $this->razonSocial . "<br></p><p>Numero de Licencia: " . $this->numLicencia . "<br></p>";
+        }
+    }
+
+?>
