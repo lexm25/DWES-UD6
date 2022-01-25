9d5fe443e09fa1955035e16371c2f086ac3097cd
lexm25 <alejandro.martin.alvarez.al@iespoligonosur.org>
2022-01-25 13:36:46 +0100

    clase

--- ejercicio11/controllers/libros_controller.php
+++ ejercicio11/controllers/libros_controller.php
@@ -1,10 +1,4 @@
 <?php 
-    //http://localhost/DWES22/DWES-UD6/ejercicio11/index.php?controller=libros&action=listar
-    function listar(){
-        require 'models/libros_model.php';
-        $libros = getLibros();
-        include 'views/libros_listar.php';    
-    }
     //http://localhost/DWES22/DWES-UD6/ejercicio11/index.php?controller=libros&action=mostrarLibro&id=2
     function mostrarLibro(){
         require 'models/libros_model.php';
--- ejercicio11/models/libros_model.php
+++ ejercicio11/models/libros_model.php
@@ -5,16 +5,6 @@
         return new PDO("mysql:host=localhost;dbname=biblioteca",$user,$pwd);
     }
 
-    function getLibros(){
-        $db = getConnection();
-        $result = $db->query("SELECT titulo, precio FROM libros");
-        $libros = array();
-        while($libro = $result->fetch()){
-            $libros[] = $libro;
-        }
-        return $libros;
-    }
-
     function getLibro($id){
         $db = getConnection();
         $result = $db->prepare("SELECT titulo, precio FROM libros WHERE id=?");
